cd1e184a2e478dcfbff47c3e0590e40b3c1d017b
Nathan Downer <nathandowner123@gmail.com>
2019-08-10 08:46:05 -0500

    add tablename to users table

--- api/model/User.py
+++ api/model/User.py
@@ -2,6 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash
 
 class User(db.Model):
+  __tablename__ = "users"
+
   id = db.Column(db.Integer, primary_key=True)
   firstname = db.Column(db.String(20), nullable=False)
   lastname = db.Column(db.String(20), nullable=False)
@@ -18,3 +20,22 @@ class User(db.Model):
  
   def __repr__(self):
     return f"<User {self.firstname}>"
+
+  user_groups = db.Table('user_groups',
+  db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+  db.Column('group_id', db.Integer, db.ForeignKey('groups.id'), primary_key=True)
+)
+
+class Group(db.Model):
+  __tablename__ = "groups"
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(120), nullable=False)
+  date_created = db.Column(db.Date, nullable=False, default=datetime.datetime.utcnow())
+  members = db.relationship('User', secondary=user_groups, lazy=True,
+    backref=db.backref('groups'))
+
+  def __init__(self, name):
+    self.name = name
+
+  def __repr__(self):
+    return f"<Group {name}>"
