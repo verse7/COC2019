2b9be781daebd6b4fed368a349ad078e6e000810
Nathan Downer <nathandowner123@gmail.com>
2019-08-10 00:02:04 -0500

    update registration form

--- api/form/RegistrationForm.py
+++ api/form/RegistrationForm.py
@@ -5,5 +5,7 @@ from wtforms.validators import InputRequired, Email
 class RegistrationForm(FlaskForm):
   class Meta:
     csrf = False
+  firstname = StringField('firstname', validators=[InputRequired()])
+  lastname = StringField('lastname', validators=[InputRequired()])
   email = StringField('email', validators=[Email(), InputRequired()])
   password = StringField('password', validators=[InputRequired()])
