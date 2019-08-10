ba5e1e16695161e6e2fce217bbac8f1b11934b9d
Nathan Downer <nathandowner123@gmail.com>
2019-08-09 23:49:35 -0500

    update fields in user model

--- api/model/User.py
+++ api/model/User.py
@@ -3,9 +3,18 @@ from werkzeug.security import generate_password_hash
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+  firstname = db.Column(db.String(20), nullable=False)
+  lastname = db.Column(db.String(20), nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(255), nullable=False)
+  points = db.Column(db.Integer, nullable=False)
 
-  def __init__(self, email, password):
+  def __init__(self, firstname, lastname, email, password):
+    self.firstname = firstname
+    self.lastname = lastname
     self.email = email
     self.password = generate_password_hash(password, method='pbkdf2:sha256')
+    self.points = 0
+ 
+  def __repr__(self):
+    return f"<User {self.firstname}>"
