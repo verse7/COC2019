179d1bc3f0b82e4c4e7c41ec95286d74b123cdee
Zidane <zidane.wright@speurgroup.com>
2019-08-10 21:52:19 -0500

    add websocket functionality

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from api import create_api
+from api.ws import socketio
 
 if __name__ == '__main__':
   api = create_api()
-  api.run(debug=True, host="0.0.0.0", port="5000")
+  socketio.run(api, host="0.0.0.0", port=5000)
